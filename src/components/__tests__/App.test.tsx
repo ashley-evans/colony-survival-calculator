72e187b39509709e415681a052df01afe0871c81
Ashley Evans <ashleyev@hotmail.co.uk>
2023-01-20 21:10:28 +0000

    Update App.tsx to fetch all known static items

--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
+import { waitForRequest } from "./helpers/utils";
 import App from "../App";
+
+const expectedItemPath = "json/items.json";
+
+const server = setupServer(
+    rest.get(expectedItemPath, (_, res, ctx) => {
+        return res(ctx.status(200));
+    })
+);
+
+beforeAll(() => {
+    server.listen();
+});
+
+beforeEach(() => {
+    server.resetHandlers();
+});
 
 test("renders application header", async () => {
     const expectedHeader = "Colony Survival Calculator";
@@ -11,4 +30,16 @@ test("renders application header", async () => {
     expect(
         await screen.findByRole("heading", { name: expectedHeader })
     ).toBeVisible();
+});
+
+test("fetches all known static items", async () => {
+    const expectedRequest = waitForRequest(server, "GET", "json/items.json");
+
+    render(<App />);
+
+    await expect(expectedRequest).resolves.not.toThrowError();
+});
+
+afterAll(() => {
+    server.close();
 });
