66af9bde54ab4646346657a4eb9749fd2e758f1e
Ashley Evans <ashleyev@hotmail.co.uk>
2023-01-20 22:19:27 +0000

    Update App.tsx to validate static items and render error if invalid or missing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Ajv from "ajv";
+
+import { Items } from "../types";
+import ItemsSchema from "../schemas/items.json";
+
+const ajv = new Ajv();
+const validateItems = ajv.compile<Items>(ItemsSchema);
 
 function App() {
+    const [items, setItems] = useState<Items>();
+
     useEffect(() => {
-        axios.get("json/items.json");
+        axios.get<unknown>("json/items.json").then(({ data }) => {
+            if (validateItems(data)) {
+                setItems(data);
+            } else {
+                setItems([]);
+            }
+        });
     }, []);
 
-    return <h1>Colony Survival Calculator</h1>;
+    return (
+        <>
+            <h1>Colony Survival Calculator</h1>
+            <h2>Desired output:</h2>
+            {items && items.length == 0 ? (
+                <p role="alert">Error: Unable to fetch known items</p>
+            ) : null}
+        </>
+    );
 }
 
 export default App;
--- src/components/__tests__/App.test.tsx
+++ src/components/__tests__/App.test.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
 
-import { waitForRequest } from "./helpers/utils";
+import { waitForRequest } from "../../helpers/utils";
 import App from "../App";
+import { Items } from "../../types";
 
-const expectedItemPath = "json/items.json";
+const STATIC_ITEMS_PATH = "json/items.json";
+const VALID_ITEMS: Items = [
+    { name: "Test Item 1", createTime: 2 },
+    { name: "Test Item 2", createTime: 4 },
+];
 
 const server = setupServer(
-    rest.get(expectedItemPath, (_, res, ctx) => {
-        return res(ctx.status(200));
+    rest.get(STATIC_ITEMS_PATH, (_, res, ctx) => {
+        return res(ctx.json(VALID_ITEMS));
     })
 );
 
@@ -32,12 +37,64 @@ test("renders application header", async () => {
     ).toBeVisible();
 });
 
-test("fetches all known static items", async () => {
-    const expectedRequest = waitForRequest(server, "GET", "json/items.json");
+describe("output selector", () => {
+    test("fetches all known static items", async () => {
+        const expectedRequest = waitForRequest(
+            server,
+            "GET",
+            STATIC_ITEMS_PATH
+        );
 
-    render(<App />);
+        render(<App />);
 
-    await expect(expectedRequest).resolves.not.toThrowError();
+        await expect(expectedRequest).resolves.not.toThrowError();
+    });
+
+    test("renders desired output header", async () => {
+        const expectedHeader = "Desired output:";
+
+        render(<App />);
+
+        expect(
+            await screen.findByRole("heading", { name: expectedHeader })
+        ).toBeVisible();
+    });
+
+    test.each([
+        ["no static items returned", []],
+        ["invalid static items returned", [{ invalid: "invalid" }]],
+    ])(
+        "renders a missing known items error if %s",
+        async (_: string, items) => {
+            server.use(
+                rest.get(STATIC_ITEMS_PATH, (_, res, ctx) => {
+                    return res(ctx.json(items));
+                })
+            );
+
+            const expectedError = "Error: Unable to fetch known items";
+
+            render(<App />);
+
+            expect(await screen.findByRole("alert")).toHaveTextContent(
+                expectedError
+            );
+        }
+    );
+
+    test("does not render a missing known items if static items are returned", async () => {
+        const expectedRequest = waitForRequest(
+            server,
+            "GET",
+            STATIC_ITEMS_PATH
+        );
+        const errorMessage = "Error: Unable to fetch known items";
+
+        render(<App />);
+        await waitFor(() => expectedRequest);
+
+        expect(screen.queryByText(errorMessage)).not.toBeInTheDocument();
+    });
 });
 
 afterAll(() => {
