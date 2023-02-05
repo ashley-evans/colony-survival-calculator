001214fb58286a9ba5f36e4b9102b999df2cf20c
Ashley Evans <ashleyev@hotmail.co.uk>
2023-02-05 22:19:46 +0000

    Add IAM policy for OIDC and policy required for working with s3 backend

---
+++ infra/deploy/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  backend "s3" {
+    key                  = "deploy/terraform.tfstate"
+    workspace_key_prefix = ""
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+
+  required_version = ">= 1.3.7"
+}
+
+data "aws_iam_policy_document" "deploy_policy_document" {
+  statement {
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [var.gha_oidc_provider_arn]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_organization}/${var.repository_name}:*"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "remote_state_read_write_policy" {
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:ListBucket"
+        Effect   = "Allow"
+        Resource = var.remote_state_bucket_arn
+      },
+      {
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+        Effect   = "Allow"
+        Resource = "${var.remote_state_bucket_arn}/*"
+      },
+      {
+        Action = [
+          "dynamodb:DescribeTable",
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ],
+        Effect   = "Allow"
+        Resource = var.remote_state_table_arn
+      }
+    ]
+  })
+}
---
+++ infra/deploy/variables.tf
@@ -0,0 +1,19 @@
+variable "gha_oidc_provider_arn" {
+  type = string
+}
+
+variable "github_organization" {
+  type = string
+}
+
+variable "repository_name" {
+  type = string
+}
+
+variable "remote_state_bucket_arn" {
+  type = string
+}
+
+variable "remote_state_table_arn" {
+  type = string
+}
