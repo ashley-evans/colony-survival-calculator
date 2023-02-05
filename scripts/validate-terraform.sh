291264b6431c2b1e74d54059e4afca6b0be76047
Ashley Evans <ashleyev@hotmail.co.uk>
2023-02-05 22:33:25 +0000

    Update ci.yml to assume ui deploy role on validate step

--- scripts/validate-terraform.sh
+++ scripts/validate-terraform.sh
@@ -6,7 +6,12 @@ root_dir="$( dirname "$script_dir")"
 folders=$(find $root_dir -type f -name '*.tf' -exec dirname {} \; | uniq)
 for folder in $folders; do
     echo "Ensuring terraform is initialised..."
-    terraform -chdir=$folder init -input=false > /dev/null
+    terraform -chdir=$folder init -input=false -backend-config="../remote-state.tfbackend" > /dev/null
+
+    exit_code=$(echo $?)
+    if [ $exit_code -ne 0 ]; then
+        exit $exit_code
+    fi
 
     echo "Validating all terraform files in $folder..."
     terraform -chdir=$folder validate
