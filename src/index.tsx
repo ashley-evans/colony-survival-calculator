eba3b8a365b7fe32c80a240e80c1f7fdd648dfdd
Ashley Evans <ashleyev@hotmail.co.uk>
2023-02-08 23:14:04 +0000

    Update index.tsx to render router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import Calculator from "./components/Calculator";
+import router from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <Calculator />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
