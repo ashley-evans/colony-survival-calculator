8642492970699457f4eefc6a4cbfbf92f76d3aa2
Ashley Evans <ashleyev@hotmail.co.uk>
2023-02-26 15:39:57 +0000

    Update prettier and eslint config to enable common config

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,13 +1,5 @@
 module.exports = {
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "prettier",
-    ],
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint"],
-    root: true,
+    extends: ["../.eslintrc.js", "plugin:react/recommended"],
     env: {
         browser: true,
         node: true,
