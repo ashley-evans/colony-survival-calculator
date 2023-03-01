43315837b7974b9e541ad1853780c6bf6ecb7da1
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-01 22:22:36 +0000

    Update API deploy script to run build steps regardless of terraform action

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -32,6 +32,8 @@ while getopts "e:r:dth" opt; do
     esac
 done
 
+set -e
+
 if [ -z $environment ]; then
     environment="dev"
 fi
@@ -51,19 +53,26 @@ echo "Ensuring terraform is initialised..."
 
 terraform -chdir="$infra_dir" init -input=false -backend-config="$repository_dir/infra/remote-state.tfbackend" > /dev/null
 
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    exit $exit_code
-fi
-
 echo "Switching to environment: $environment"
 
 terraform -chdir="$infra_dir" workspace select $environment
 
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    exit $exit_code
-fi
+echo "Building API..."
+npm --prefix $script_parent_dir run build:clean
+
+echo "Copying package definitions..."
+$script_parent_dir/node_modules/.bin/copyfiles -E -u 1 \
+    -e "$src_dir/**/node_modules/**" \
+    "$src_dir/**/package*.json" \
+    $dist_dir
+cp $script_parent_dir/package.json $dist_dir/package.json
+
+echo "Installing dependencies..."
+cp $script_parent_dir/lerna.dist.json $dist_dir/lerna.json
+cd $dist_dir && npx lerna bootstrap --ci -- --production
+
+echo "Bundle code for deployment..."
+cd $dist_dir && npx lerna exec -- $script_parent_dir/node_modules/.bin/esbuild handler.js --bundle --platform=node --target=$runtime --outfile=dist/index.js
 
 if [ $dryrun ]; then
     echo "Dry run deployment of UI for environment: $environment..."
@@ -72,23 +81,6 @@ elif [ $teardown ]; then
     echo "Tearing down UI for environment: $environment..."
     terraform -chdir="$infra_dir" apply -var-file="$infra_dir/$environment.tfvars" -destroy
 else
-    echo "Building API..."
-    npm --prefix $script_parent_dir run build:clean
-
-    echo "Copying package definitions..."
-    $script_parent_dir/node_modules/.bin/copyfiles -E -u 1 \
-        -e "$src_dir/**/node_modules/**" \
-        "$src_dir/**/package*.json" \
-        $dist_dir
-    cp $script_parent_dir/package.json $dist_dir/package.json
-
-    echo "Installing dependencies..."
-    cp $script_parent_dir/lerna.dist.json $dist_dir/lerna.json
-    cd $dist_dir && npx lerna bootstrap --ci -- --production
-
-    echo "Bundle code for deployment..."
-    cd $dist_dir && npx lerna exec -- $script_parent_dir/node_modules/.bin/esbuild handler.js --bundle --platform=node --target=$runtime --outfile=dist/index.js
-
     echo "Deploying UI for environment: $environment..."
     terraform -chdir="$infra_dir" apply -auto-approve -var-file="$infra_dir/$environment.tfvars"
 fi
