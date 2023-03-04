f7be9aa06fc8c4d2e40ec36ab46cfc8df8812c72
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-04 00:41:51 +0000

    feat(api): update add item handler to receive and validate s3 notification and request object detail
    Includes addition of test util functions to create mock s3 events

--- api/src/functions/add-item/__tests__/handler.test.ts
+++ api/src/functions/add-item/__tests__/handler.test.ts
@@ -1,26 +1,105 @@
-import type { AppSyncResolverEvent } from "aws-lambda";
-import { mock } from "jest-mock-extended";
+import type { S3EventRecord } from "aws-lambda";
+import {
+    S3Client,
+    GetObjectCommand,
+    GetObjectCommandInput,
+} from "@aws-sdk/client-s3";
+import { mockClient } from "aws-sdk-client-mock";
 
-import type { ItemInput } from "../../../graphql/schema";
+import {
+    createS3Event,
+    createS3EventBucketDetails,
+    createS3EventBucketObjectDetails,
+    createS3EventNotificationDetails,
+    createS3EventRecord,
+} from "../../../../test";
+
 import { handler } from "../handler";
 
-function createEvent(name: string): AppSyncResolverEvent<ItemInput> {
-    const event = mock<AppSyncResolverEvent<ItemInput>>();
-    event.arguments = {
-        name,
-    };
-    return event;
-}
+const mockS3Client = mockClient(S3Client);
 
 beforeAll(() => {
     jest.spyOn(console, "log").mockImplementation(() => undefined);
 });
 
-test("returns the provided name as ID in response", async () => {
-    const expected = "test";
-    const event = createEvent(expected);
+const EXPECTED_BUCKET_NAME = "test_bucket_name";
+const EXPECTED_KEY = "seeds/items.json";
 
-    const actual = await handler(event);
+const validRecord = createS3EventRecord(
+    createS3EventNotificationDetails(
+        createS3EventBucketDetails(EXPECTED_BUCKET_NAME),
+        createS3EventBucketObjectDetails(EXPECTED_KEY)
+    )
+);
 
-    expect(actual.id).toEqual(expected);
+test.each([
+    ["undefined records are provided", undefined],
+    ["no records are provided", []],
+    ["a record with missing notification details", [createS3EventRecord()]],
+    [
+        "a record with missing bucket details",
+        [
+            createS3EventRecord(
+                createS3EventNotificationDetails(
+                    undefined,
+                    createS3EventBucketObjectDetails(EXPECTED_KEY)
+                )
+            ),
+        ],
+    ],
+    [
+        "a record with missing bucket name",
+        [
+            createS3EventRecord(
+                createS3EventNotificationDetails(
+                    createS3EventBucketDetails(),
+                    createS3EventBucketObjectDetails(EXPECTED_KEY)
+                )
+            ),
+        ],
+    ],
+    [
+        "a record with missing bucket object details",
+        [
+            createS3EventRecord(
+                createS3EventNotificationDetails(
+                    createS3EventBucketDetails(EXPECTED_BUCKET_NAME),
+                    undefined
+                )
+            ),
+        ],
+    ],
+    [
+        "a record with missing bucket object key",
+        [
+            createS3EventRecord(
+                createS3EventNotificationDetails(
+                    createS3EventBucketDetails(EXPECTED_BUCKET_NAME),
+                    createS3EventBucketObjectDetails()
+                )
+            ),
+        ],
+    ],
+])(
+    "throws an error if an event with %s",
+    async (_: string, records?: S3EventRecord[]) => {
+        const event = createS3Event(records);
+
+        expect.assertions(1);
+        await expect(handler(event)).rejects.toMatchSnapshot();
+    }
+);
+
+test("calls S3 to get item JSON details given single valid record", async () => {
+    const event = createS3Event([validRecord]);
+
+    await handler(event);
+
+    const calls = mockS3Client.commandCalls(GetObjectCommand);
+    expect(calls).toHaveLength(1);
+    expect(calls[0]?.args).toHaveLength(1);
+
+    const input = calls[0]?.args[0].input as GetObjectCommandInput;
+    expect(input.Bucket).toEqual(EXPECTED_BUCKET_NAME);
+    expect(input.Key).toEqual(EXPECTED_KEY);
 });
--- api/src/functions/add-item/handler.ts
+++ api/src/functions/add-item/handler.ts
@@ -1,13 +1,75 @@
-import dayjs from "dayjs";
+import Ajv, { JSONSchemaType } from "ajv";
+import {
+    S3Client,
+    GetObjectCommand,
+    GetObjectCommandInput,
+} from "@aws-sdk/client-s3";
 
-import type { GraphQLEventHandler } from "../../interfaces/GraphQLEventHandler";
-import type { Item, ItemInput } from "../../graphql/schema";
+import type { S3EventHandler } from "../../interfaces/S3EventHandler";
 
-const handler: GraphQLEventHandler<ItemInput, Item> = async (event) => {
-    console.log(`The date is: ${dayjs().format()}`);
-    return {
-        id: event.arguments.name,
+type ValidS3EventRecord = {
+    s3: {
+        bucket: {
+            name: string;
+        };
+        object: {
+            key: string;
+        };
     };
+};
+
+const eventSchema: JSONSchemaType<ValidS3EventRecord> = {
+    type: "object",
+    properties: {
+        s3: {
+            type: "object",
+            properties: {
+                bucket: {
+                    type: "object",
+                    properties: {
+                        name: { type: "string" },
+                    },
+                    required: ["name"],
+                },
+                object: {
+                    type: "object",
+                    properties: {
+                        key: { type: "string" },
+                    },
+                    required: ["key"],
+                },
+            },
+            required: ["bucket", "object"],
+        },
+    },
+    required: ["s3"],
+};
+
+const ajv = new Ajv();
+const validateEventRecord = ajv.compile<ValidS3EventRecord>(eventSchema);
+
+const client = new S3Client({});
+
+const handler: S3EventHandler<void> = async (event) => {
+    if (!event.Records || event.Records.length === 0) {
+        throw "No event records provided";
+    }
+
+    const record = event.Records[0];
+    if (!record) {
+        throw "";
+    }
+
+    if (validateEventRecord(record)) {
+        const input: GetObjectCommandInput = {
+            Key: record.s3.object.key,
+            Bucket: record.s3.bucket.name,
+        };
+
+        await client.send(new GetObjectCommand(input));
+    } else {
+        throw validateEventRecord.errors;
+    }
 };
 
 export { handler };
