fb49e87e1dc0c06eb7aaf0388e19b1e81230dc26
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-07 20:21:37 +0000

    feat(api): update add item domain logic to validate items against schema
    includes basic call to secondary adapter to store item in db

--- api/src/functions/add-item/domain/add-item.ts
+++ api/src/functions/add-item/domain/add-item.ts
@@ -1,7 +1,29 @@
+import Ajv from "ajv";
+
+import ItemsSchema from "../../../json/schemas/items.json";
+import type { Items } from "../../../types";
+import { storeItem } from "../adapters/store-item";
 import type { AddItemPrimaryPort } from "../interfaces/add-item-primary-port";
 
+const ajv = new Ajv();
+const validateItems = ajv.compile<Items>(ItemsSchema);
+
+function parseItems(input: string): Items {
+    try {
+        const parsed = JSON.parse(input);
+        if (validateItems(parsed)) {
+            return parsed;
+        } else {
+            throw validateItems.errors;
+        }
+    } catch (ex) {
+        throw `Validation Error: ${ex}`;
+    }
+}
+
 const addItem: AddItemPrimaryPort = async (items) => {
-    console.log(items);
+    const parsedItems = parseItems(items);
+    await storeItem(parsedItems);
     return true;
 };
 
