f9da97459a3da00c9fb6b42f026234840aea95ee
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-11 02:28:05 +0000

    feat(api): update add item lambda to connect and store items in mongodb

---
+++ api/src/functions/add-item/adapters/__tests__/store-item.test.ts
@@ -0,0 +1,150 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import { MongoClient } from "mongodb";
+
+import { createItem } from "../../../../../test/index";
+import type { Items } from "../../../../types";
+
+const databaseName = "TestDatabase";
+const itemCollectionName = "Items";
+
+let mongoDBMemoryServer: MongoMemoryServer;
+let connection: MongoClient;
+
+function getItemsCollection() {
+    const db = connection.db(databaseName);
+    return db.collection(itemCollectionName);
+}
+
+async function clearItemsCollection() {
+    const db = connection.db(databaseName);
+
+    const existingCollections = await db.listCollections().toArray();
+    const collectionExists =
+        existingCollections.find(
+            (collection) => collection.name == itemCollectionName
+        ) !== undefined;
+    if (collectionExists) {
+        const itemsCollection = db.collection(itemCollectionName);
+        await itemsCollection.drop();
+    }
+}
+
+beforeAll(async () => {
+    mongoDBMemoryServer = await MongoMemoryServer.create({
+        binary: {
+            version: "6.0.4",
+        },
+        instance: {
+            dbName: databaseName,
+        },
+    });
+
+    connection = await MongoClient.connect(mongoDBMemoryServer.getUri());
+}, 10000);
+
+beforeEach(async () => {
+    process.env["DATABASE_NAME"] = databaseName;
+    process.env["ITEM_COLLECTION_NAME"] = "Items";
+    process.env["MONGO_DB_URI"] = mongoDBMemoryServer.getUri();
+    process.env["AWS_ACCESS_KEY_ID"] = "test_access_key_id";
+    process.env["AWS_SECRET_ACCESS_KEY"] = "test_secret_access_key";
+    process.env["TEST_ENV"] = "true";
+
+    await clearItemsCollection();
+});
+
+test.each([
+    [
+        "database name",
+        "DATABASE_NAME",
+        "Misconfigured: Database name not provided",
+    ],
+    [
+        "item collection name",
+        "ITEM_COLLECTION_NAME",
+        "Misconfigured: Item collection name not provided",
+    ],
+    [
+        "mongodb URI",
+        "MONGO_DB_URI",
+        "Misconfigured: URI for MongoDB not provided",
+    ],
+    [
+        "AWS access key ID",
+        "AWS_ACCESS_KEY_ID",
+        "Misconfigured: AWS Access Key ID not provided",
+    ],
+    [
+        "AWS secret access key",
+        "AWS_SECRET_ACCESS_KEY",
+        "Misconfigured: AWS Secret Access Key not provided",
+    ],
+])(
+    "throws an error if %s configuration not provided",
+    async (_: string, key: string, expectedError: string) => {
+        delete process.env["TEST_ENV"];
+        delete process.env[key];
+
+        expect.assertions(1);
+        await expect(async () => {
+            await import("../store-item");
+        }).rejects.toThrow(expectedError);
+    }
+);
+
+describe("empty array handling", () => {
+    test("returns success if provided an empty array", async () => {
+        const { storeItem } = await import("../store-item");
+
+        const actual = await storeItem([]);
+
+        expect(actual).toBe(true);
+    });
+
+    test("adds no items to the items collection", async () => {
+        const { storeItem } = await import("../store-item");
+
+        await storeItem([]);
+
+        const itemsCollection = getItemsCollection();
+        const items = await itemsCollection.find().toArray();
+        expect(items).toHaveLength(0);
+    });
+});
+
+describe.each([
+    [
+        "a single item",
+        [createItem("test item 1", 2, 3, [{ name: "test", amount: 1 }])],
+    ],
+    [
+        "multiple items",
+        [
+            createItem("test item 1", 2, 3, [{ name: "test", amount: 1 }]),
+            createItem("test item 2", 1, 4, [{ name: "world", amount: 3 }]),
+        ],
+    ],
+])("handles adding %s", (_: string, expected: Items) => {
+    test("returns success", async () => {
+        const { storeItem } = await import("../store-item");
+
+        const actual = await storeItem(expected);
+
+        expect(actual).toBe(true);
+    });
+
+    test("adds the provided item(s) to the item collection", async () => {
+        const { storeItem } = await import("../store-item");
+
+        await storeItem(expected);
+
+        const itemsCollection = getItemsCollection();
+        const items = await itemsCollection.find().toArray();
+        expect(items).toHaveLength(items.length);
+        expect(items).toEqual(expect.arrayContaining(expected));
+    });
+});
+
+afterAll(async () => {
+    await mongoDBMemoryServer.stop();
+});
--- api/src/functions/add-item/adapters/store-item.ts
+++ api/src/functions/add-item/adapters/store-item.ts
@@ -1,7 +1,56 @@
+import { MongoClient } from "mongodb";
+
 import type { StoreItemPort } from "../interfaces/store-item-port";
 
+const databaseName = process.env["DATABASE_NAME"];
+if (!databaseName) {
+    throw new Error("Misconfigured: Database name not provided");
+}
+
+const itemCollectionName = process.env["ITEM_COLLECTION_NAME"];
+if (!itemCollectionName) {
+    throw new Error("Misconfigured: Item collection name not provided");
+}
+
+const mongoURI = process.env["MONGO_DB_URI"];
+if (!mongoURI) {
+    throw new Error("Misconfigured: URI for MongoDB not provided");
+}
+
+const accessKeyID = process.env["AWS_ACCESS_KEY_ID"];
+if (!accessKeyID) {
+    throw new Error("Misconfigured: AWS Access Key ID not provided");
+}
+
+const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"];
+if (!secretAccessKey) {
+    throw new Error("Misconfigured: AWS Secret Access Key not provided");
+}
+
+const client = MongoClient.connect(mongoURI, {
+    ...(process.env["TEST_ENV"]
+        ? {}
+        : {
+              auth: {
+                  username: accessKeyID,
+                  password: secretAccessKey,
+              },
+              authSource: "$external",
+              authMechanism: "MONGODB-AWS",
+          }),
+});
+
 const storeItem: StoreItemPort = async (items) => {
-    console.dir(items);
+    if (items.length === 0) {
+        return true;
+    }
+
+    const connection = await client;
+    const db = connection.db(databaseName);
+    const collection = db.collection(itemCollectionName);
+
+    await collection.insertMany(items);
+
     return true;
 };
 
