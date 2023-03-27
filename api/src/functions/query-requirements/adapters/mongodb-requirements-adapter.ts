c6c39d35b72075ca869776d075a02ce601eaeba5
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-27 01:43:37 +0100

    feat(api): update query reqs mongodb adapter to traverse graph to find all relevant req details

--- api/src/functions/query-requirements/adapters/mongodb-requirements-adapter.ts
+++ api/src/functions/query-requirements/adapters/mongodb-requirements-adapter.ts
@@ -1,7 +1,62 @@
+import client from "@colony-survival-calculator/mongodb-client";
+
+import type { Item } from "../../../types";
 import type { RequirementsDatabasePort } from "../interfaces/requirements-database-port";
 
-const queryRequirements: RequirementsDatabasePort = async () => {
-    return [];
+const databaseName = process.env["DATABASE_NAME"];
+if (!databaseName) {
+    throw new Error("Misconfigured: Database name not provided");
+}
+
+const itemCollectionName = process.env["ITEM_COLLECTION_NAME"];
+if (!itemCollectionName) {
+    throw new Error("Misconfigured: Item collection name not provided");
+}
+
+const queryRequirements: RequirementsDatabasePort = async (name: string) => {
+    const db = (await client).db(databaseName);
+    const collection = db.collection<Item>(itemCollectionName);
+
+    const requirements = collection.aggregate<Item>([
+        {
+            $match: {
+                name,
+            },
+        },
+        {
+            $graphLookup: {
+                from: itemCollectionName,
+                startWith: "$name",
+                connectFromField: "requires.name",
+                connectToField: "name",
+                as: "result",
+                depthField: "depth",
+            },
+        },
+        {
+            $unwind: {
+                path: "$result",
+            },
+        },
+        {
+            $replaceRoot: {
+                newRoot: "$result",
+            },
+        },
+        {
+            $sort: {
+                depth: 1,
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                depth: 0,
+            },
+        },
+    ]);
+
+    return requirements.toArray();
 };
 
 export { queryRequirements };
