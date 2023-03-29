ae697668638d6dd9c4f7c161dadb9d339f63dce7
Ashley Evans <ashleyev@hotmail.co.uk>
2023-03-30 00:20:58 +0100

    feat(api): update query output mongodb adapter to fetch create time and output amount from db

--- api/src/functions/query-output/adapters/mongodb-output-adapter.ts
+++ api/src/functions/query-output/adapters/mongodb-output-adapter.ts
@@ -1,7 +1,29 @@
-import type { OutputDatabasePort } from "../interfaces/output-database-port";
+import client from "@colony-survival-calculator/mongodb-client";
 
-const queryOutputDetails: OutputDatabasePort = async () => {
-    return [];
+import type { Item } from "../../../types";
+import type {
+    ItemOutputDetails,
+    OutputDatabasePort,
+} from "../interfaces/output-database-port";
+
+const databaseName = process.env["DATABASE_NAME"];
+if (!databaseName) {
+    throw new Error("Misconfigured: Database name not provided");
+}
+
+const itemCollectionName = process.env["ITEM_COLLECTION_NAME"];
+if (!itemCollectionName) {
+    throw new Error("Misconfigured: Item collection name not provided");
+}
+
+const queryOutputDetails: OutputDatabasePort = async (name) => {
+    const db = (await client).db(databaseName);
+    const collection = db.collection(itemCollectionName);
+
+    return collection
+        .find<Item>({ name })
+        .project<ItemOutputDetails>({ _id: 0, createTime: 1, output: 1 })
+        .toArray();
 };
 
 export { queryOutputDetails };
