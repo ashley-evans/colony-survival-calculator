2b3ca091ca36d9dfb25147096f3213bb6a6193dc
Ashley Evans <ashleyev@hotmail.co.uk>
2023-04-02 23:47:50 +0100

    chore: update commit lint configuration to ignore dependabot commits
    Reverts changes to dependabot configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from "@commitlint/types";
 
 const configuration: UserConfig = {
     extends: ["@commitlint/config-conventional"],
+    ignores: [
+        (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+    ],
 };
 
 module.exports = configuration;
