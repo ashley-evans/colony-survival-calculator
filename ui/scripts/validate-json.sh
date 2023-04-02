51977b4d62f9447144cd249dd7fe7eae346cbc4c
Ashley Evans <ashleyev@hotmail.co.uk>
2023-04-02 13:05:08 +0100

    build(ui): remove JSON schema validation scripts

--- ui/scripts/validate-json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-root_dir="$( dirname "$script_dir")"
-
-schema_folder="$root_dir/src/schemas"
-static_json_folder="$root_dir/public/json"
-
-npx --yes ajv-cli -s "$schema_folder/items.json" -d "$static_json_folder/items.json"
