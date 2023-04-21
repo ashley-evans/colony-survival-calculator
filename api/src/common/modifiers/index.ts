97a4cc1eea0c7bd910fac4edcf090d6502809adb
Ashley Evans <ashleyev@hotmail.co.uk>
2023-04-21 18:17:35 +0100

    feat(api): update requirements query app sync adapter to handle conversion between tool schemas

--- api/src/common/modifiers/index.ts
+++ api/src/common/modifiers/index.ts
@@ -1,21 +1,37 @@
-import { Tools } from "../../types";
+import { Tools as GraphQLSchemaTools } from "../../graphql/schema";
+import { Tools as JSONSchemaTools } from "../../types";
 
-const ToolModifierValues: Readonly<Record<Tools, number>> = {
-    [Tools.none]: 1,
-    [Tools.stone]: 2,
-    [Tools.copper]: 4,
-    [Tools.iron]: 5.3,
-    [Tools.bronze]: 6.15,
-    [Tools.steel]: 8,
+const ToolModifierValues: Readonly<Record<JSONSchemaTools, number>> = {
+    [JSONSchemaTools.none]: 1,
+    [JSONSchemaTools.stone]: 2,
+    [JSONSchemaTools.copper]: 4,
+    [JSONSchemaTools.iron]: 5.3,
+    [JSONSchemaTools.bronze]: 6.15,
+    [JSONSchemaTools.steel]: 8,
 };
 
-function isAvailableToolSufficient(minimum: Tools, available: Tools): boolean {
+const ToolSchemaMap: Record<GraphQLSchemaTools, JSONSchemaTools> = {
+    NONE: JSONSchemaTools.none,
+    STONE: JSONSchemaTools.stone,
+    COPPER: JSONSchemaTools.copper,
+    IRON: JSONSchemaTools.iron,
+    BRONZE: JSONSchemaTools.bronze,
+    STEEL: JSONSchemaTools.steel,
+};
+
+function isAvailableToolSufficient(
+    minimum: JSONSchemaTools,
+    available: JSONSchemaTools
+): boolean {
     const minimumToolModifier = ToolModifierValues[minimum];
     const availableToolModifier = ToolModifierValues[available];
     return availableToolModifier >= minimumToolModifier;
 }
 
-function getMaxToolModifier(maximum: Tools, available: Tools): number {
+function getMaxToolModifier(
+    maximum: JSONSchemaTools,
+    available: JSONSchemaTools
+): number {
     const maximumToolModifier = ToolModifierValues[maximum];
     const availableToolModifier = ToolModifierValues[available];
     return availableToolModifier > maximumToolModifier
@@ -23,4 +39,9 @@ function getMaxToolModifier(maximum: Tools, available: Tools): number {
         : availableToolModifier;
 }
 
-export { ToolModifierValues, isAvailableToolSufficient, getMaxToolModifier };
+export {
+    ToolSchemaMap,
+    ToolModifierValues,
+    isAvailableToolSufficient,
+    getMaxToolModifier,
+};
--- api/src/functions/query-requirements/handler.ts
+++ api/src/functions/query-requirements/handler.ts
@@ -1,3 +1,4 @@
+import { ToolSchemaMap } from "../../common/modifiers";
 import type { QueryRequirementArgs, Requirement } from "../../graphql/schema";
 import type { GraphQLEventHandler } from "../../interfaces/GraphQLEventHandler";
 import { queryRequirements } from "./domain/query-requirements";
@@ -6,9 +7,13 @@ const handler: GraphQLEventHandler<
     QueryRequirementArgs,
     Requirement[]
 > = async (event) => {
-    const { name, workers } = event.arguments;
+    const { name, workers, maxAvailableTool } = event.arguments;
 
-    return await queryRequirements(name, workers);
+    return await queryRequirements(
+        name,
+        workers,
+        maxAvailableTool ? ToolSchemaMap[maxAvailableTool] : undefined
+    );
 };
 
 export { handler };
