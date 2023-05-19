a527540dae0230affc3272076f44951aa9356ab6
Ashley Evans <ashleyev@hotmail.co.uk>
2023-05-19 21:48:23 +0100

    feat(api): add basic wrapping domain for distinct item names to seperate primary and secondary infra

---
+++ api/src/functions/query-distinct-item-names/adapters/mongodb-distinct-item-name-adapter.ts
@@ -0,0 +1,7 @@
+import { ItemDatabasePort } from "../interfaces/item-database-port";
+
+const queryDistinctItemNames: ItemDatabasePort = async () => {
+    return [];
+};
+
+export { queryDistinctItemNames };
--- api/src/functions/query-distinct-item-names/domain/query-distinct-item-names.ts
+++ api/src/functions/query-distinct-item-names/domain/query-distinct-item-names.ts
@@ -1,7 +1,8 @@
 import { QueryDistinctItemNamesPrimaryPort } from "../interfaces/query-distinct-item-names-primary-port";
+import { queryDistinctItemNames as dbQueryItemNames } from "../adapters/mongodb-distinct-item-name-adapter";
 
 const queryDistinctItemNames: QueryDistinctItemNamesPrimaryPort = async () => {
-    return [];
+    return dbQueryItemNames();
 };
 
 export { queryDistinctItemNames };
