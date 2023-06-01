ad4646ed0fadb5d1e95db5005ba3f7db84e3ae1d
Ashley Evans <ashleyev@hotmail.co.uk>
2023-06-01 22:36:02 +0100

    ci(api): add fp-ts lint and test configuration

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-    extends: ["../.eslintrc.js"],
+    extends: [
+        "../.eslintrc.js",
+        "plugin:functional/external-typescript-recommended",
+        "plugin:functional/lite",
+        "plugin:functional/stylistic",
+    ],
+    parserOptions: {
+        project: "./api/tsconfig.json",
+    },
+    plugins: ["@typescript-eslint", "functional"],
     env: {
         node: true,
     },
