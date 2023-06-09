b511559853989cd2096b0eec342373e3882115b4
Ashley Evans <ashleyev@hotmail.co.uk>
2023-06-09 22:53:10 +0100

    feat(api): update item query schema and appsync adapter to accept creator name filter

--- api/src/functions/query-item/interfaces/query-item-primary-port.ts
+++ api/src/functions/query-item/interfaces/query-item-primary-port.ts
@@ -3,6 +3,7 @@ import type { Items } from "../../../types";
 type QueryFilters = {
     name?: string | undefined;
     minimumCreators?: number | undefined;
+    creator?: string | undefined;
 };
 
 interface QueryItemPrimaryPort {
