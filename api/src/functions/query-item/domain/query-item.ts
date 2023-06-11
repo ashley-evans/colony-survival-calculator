5d76aa0fcb984104de1263c793712cbe3c039df4
Ashley Evans <ashleyev@hotmail.co.uk>
2023-06-11 01:35:28 +0100

    feat(api): update query-item domain logic to allow filtering items on optimal output
    defaults to using max tool for all items if no max tool is provided

--- api/src/functions/query-item/domain/query-item.ts
+++ api/src/functions/query-item/domain/query-item.ts
@@ -1,4 +1,9 @@
 import {
+    getMaxToolModifier,
+    isAvailableToolSufficient,
+} from "../../../common/modifiers";
+import { Item, Items, Tools } from "../../../types";
+import {
     queryItemByCreatorCount,
     queryItemByField,
 } from "../adapters/mongodb-query-item";
@@ -10,6 +15,47 @@ import type {
 const INVALID_FILTER_ERROR =
     "Invalid filter combination provided: Cannot filter by minimum creator and creator name";
 
+function calculateOutput(
+    item: Pick<Item, "maximumTool" | "createTime" | "output">,
+    maxAvailableTool: Tools
+): number {
+    const modifier = getMaxToolModifier(item.maximumTool, maxAvailableTool);
+    return item.output / (item.createTime / modifier);
+}
+
+function filterByOptimal(items: Items, maxAvailableTool?: Tools): Items {
+    const itemMap = new Map<string, Item>();
+    for (const item of items) {
+        if (
+            maxAvailableTool &&
+            !isAvailableToolSufficient(item.minimumTool, maxAvailableTool)
+        ) {
+            continue;
+        }
+
+        const currentOptimalItem = itemMap.get(item.name);
+        if (!currentOptimalItem) {
+            itemMap.set(item.name, item);
+            continue;
+        }
+
+        const currentOptimalOutput = calculateOutput(
+            currentOptimalItem,
+            maxAvailableTool ?? currentOptimalItem.maximumTool
+        );
+        const itemOutput = calculateOutput(
+            item,
+            maxAvailableTool ?? item.maximumTool
+        );
+
+        if (itemOutput > currentOptimalOutput) {
+            itemMap.set(item.name, item);
+        }
+    }
+
+    return Array.from(itemMap.values());
+}
+
 const queryItem: QueryItemPrimaryPort = async (
     filters: QueryFilters | undefined
 ) => {
@@ -19,14 +65,15 @@ const queryItem: QueryItemPrimaryPort = async (
     }
 
     try {
-        if (filters?.minimumCreators) {
-            return await queryItemByCreatorCount(
-                filters.minimumCreators,
-                filters.name
-            );
+        const items = await (filters?.minimumCreators
+            ? queryItemByCreatorCount(filters.minimumCreators, filters.name)
+            : queryItemByField(filters?.name, filters?.creator));
+
+        if (filters?.optimal) {
+            return filterByOptimal(items, filters.optimal.maxAvailableTool);
         }
 
-        return await queryItemByField(filters?.name, filters?.creator);
+        return items;
     } catch (ex) {
         console.error(ex);
         throw ex;
